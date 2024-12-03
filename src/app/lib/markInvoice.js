fb12bc80f91fc3ceb78db14d86fe6ab3becd0e27
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-12-03 15:10:14 -0500

    Update

--- src/app/lib/markInvoice.js
+++ src/app/lib/markInvoice.js
@@ -3,6 +3,6 @@ import { redirect } from "next/navigation";
 
 export default async function markInvoice(id) {
   "use server";
-  await sql`UPDATE client SET status = 'paid' WHERE id=${id}`;
+  await sql`UPDATE client SET status = 'Paid' WHERE id=${id}`;
   redirect("/");
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 export default async function Home() {
   const { rows } = await sql`SELECT * FROM client`;
+  const currentDate = new Date();
 
   console.log(rows);
   return (
@@ -18,14 +19,26 @@ export default async function Home() {
                 key={items.id}
                 className="bg-white mb-5 p-5 rounded flex items-center justify-between"
               >
-                <p className="text-gray-500">
-                  Due {items.duedate.toDateString()}
-                </p>
+                {items.status === "Paid" ? (
+                  <p className="text-gray-500">
+                    Paid {currentDate.toDateString()}
+                  </p>
+                ) : (
+                  <p className="text-gray-500">
+                    Due {items.duedate.toDateString()}
+                  </p>
+                )}
                 <p className="text-gray-500">{items.name}</p>
                 <p className="font-bold tracking-wide">${items.total}</p>
-                <span className="rounded-md bg-red-100 px-5 py-2 font-bold text-red-500">
-                  {items.status}
-                </span>
+                {items.status === "Paid" ? (
+                  <span className="rounded-md bg-teal-100 text-teal-500 py-2 px-5 font-bold">
+                    Paid
+                  </span>
+                ) : (
+                  <span className="rounded-md bg-amber-100 text-amber-500 px-5 py-2 font-bold">
+                    Pending
+                  </span>
+                )}
               </div>
             </Link>
           </>
