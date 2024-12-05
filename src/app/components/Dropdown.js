1f02f9c1c24d563fe07dfaca90d702a3efea9ee9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-12-04 19:10:28 -0500

    Update Dropdown.js

--- src/app/components/Dropdown.js
+++ src/app/components/Dropdown.js
@@ -5,6 +5,8 @@ import { useParams } from "next/navigation";
 
 export default function Dropdown() {
   const [isOpen, setIsOpen] = React.useState(false);
+  const { status } = useParams();
+  console.log(status);
   return (
     <div
       className="bg-white py-2 px-5 rounded-md font-bold relative cursor-pointer"
@@ -13,12 +15,28 @@ export default function Dropdown() {
       Filter by status
       {isOpen && (
         <div className="bg-white p-5 absolute inset-x-0 -bottom-32 space-y-5 rounded-md shadow-xl">
-          <Link href={"filter/paid"} className="block">
-            Paid
-          </Link>
-          <Link href={"filter/pending"} className="block">
-            Pending
-          </Link>
+          {status ? (
+            <>
+              <label className="block flex items-center gap-3">
+                <input type="checkbox" checked readOnly />
+                {status.toUpperCase()}
+              </label>
+              <Link href={"/"} className="block">
+                <button className="bg-blue-500 p-2 text-white rounded">
+                  Reset Filter
+                </button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href={"filter/paid"} className="block">
+                Paid
+              </Link>
+              <Link href={"filter/pending"} className="block">
+                Pending
+              </Link>
+            </>
+          )}
         </div>
       )}
     </div>
